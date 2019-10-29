2a943425e4ba169b1c63464d850ddcc265ac1e1a
Manishasony <56909011+Manishasony@users.noreply.github.com>
2019-10-29 15:44:19 +0530

    Update Student_Stream.java

--- Student_Stream.java
+++ Student_Stream.java
@@ -1,16 +1,17 @@
 /**
  * created by manisha
+ *sample program using string operation
  */
 package StreamOfStudent;
 import java.util.*;
 import java.util.stream.Collectors;
- class Student {
+ class Student {      //class
     String stud_name;
     String department;
     int year;
 
 
-     public Student(String stud_name,String department,int year)
+     public Student(String stud_name,String department,int year)//constructor
     {
         this.stud_name=stud_name;
         this.department=department;
@@ -37,14 +38,12 @@ import java.util.stream.Collectors;
 
 public class Student_Stream {
     public static void main(String[] args) {
-        List<Student> students = Arrays.asList(
+        List<Student> students = Arrays.asList(       //List of student details
                 new Student("kavi", "IT", 1),
                 new Student("jack", "CS", 2),
                 new Student("hema", "BCA", 3));
-        Student result = students.stream()
-
-
-                .filter(p -> "jack".equals(p.getstud_name()))
+        Student result = students.stream()             //converting list to stream
+               .filter(p -> "jack".equals(p.getstud_name()))  //filter jack only
                .findAny()
                .orElse(null);
         System.out.println("output is:" + result);
